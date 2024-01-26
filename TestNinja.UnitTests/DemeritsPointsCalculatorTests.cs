64ea02fe769bc072e0d11ff9eb8fc6910bcfaad3
rachidamrani <rachid3alaoui@gmail.com>
2024-01-26 15:49:28 +0100

    added different solutions

--- TestNinja.UnitTests/DemeritsPointsCalculatorTests.cs
+++ TestNinja.UnitTests/DemeritsPointsCalculatorTests.cs
@@ -4,36 +4,35 @@ using TestNinja.Fundamentals;
 namespace TestNinja.UnitTests;
 public class DemeritsPointsCalculatorTests
 {
-    private DemeritsPointsCalculator _demeritsPointsCalculator;
+    private DemeritsPointsCalculator _calculator;
     [SetUp]
     public void SetUp()
     {
-        _demeritsPointsCalculator = new();
+        _calculator = new();
     }
 
     [Test]
-    public void CalculateDemeritPoints_InputIsNegatif_ThrowArgumentOutOfRangeException()
+    [TestCase(-1)]
+    [TestCase(301)]
+    public void CalculateDemeritPoints_SpeedIsOutOfRange_ThrowArgumentOutOfRangeException(int speed)
     {
         Assert.That(
-            () =>
-        _demeritsPointsCalculator.CalculateDemeritPoints(-1),
-        Throws.Exception.TypeOf<ArgumentOutOfRangeException>()
+            () => _calculator.CalculateDemeritPoints(speed),
+            Throws.Exception.TypeOf<ArgumentOutOfRangeException>()
         );
     }
 
     [Test]
-    public void CalculateDemeritPoints_InputIsLessThanOrEqualSpeedLimit_ReturnZero()
+    [TestCase(0, 0)]
+    [TestCase(64, 0)]
+    [TestCase(65, 0)]
+    [TestCase(66, 0)]
+    [TestCase(70, 1)]
+    [TestCase(75, 2)]
+    public void CalculateDemeritPoints_WhenCalled_ReturnDemeritPoints(int speed, int expectedResult)
     {
-        var result = _demeritsPointsCalculator.CalculateDemeritPoints(65);
-        Assert.That(result, Is.EqualTo(0));
-    }
-
-    [Test]
-    public void CalculateDemeritPoints_InputIsGreaterThanSpeedLimit_ReturnDemeritPoints()
-    {
-        var result = _demeritsPointsCalculator.CalculateDemeritPoints(84);
-
-        Assert.That(result, Is.EqualTo(3));
+        var points = _calculator.CalculateDemeritPoints(speed);
+        Assert.That(points, Is.EqualTo(expectedResult));
     }
 
 }
